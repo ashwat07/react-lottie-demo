24c0f1cd09436dc9d30304da7a294ba723ce9bb7
ashwat07 <ashwat.acharya09@gmail.com>
2022-01-17 19:56:30 +0530

    lottie demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from "react";
+import lottie from "lottie-web";
 
 function App() {
+  const loader = useRef(null);
+  const play = useRef(null);
+  const pause = useRef(null);
+
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: loader.current,
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+      animationData: require("./Pause.json"),
+    });
+    lottie.loadAnimation({
+      container: play.current,
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+      animationData: require("./loader.json"),
+    });
+    lottie.loadAnimation({
+      container: pause.current,
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+      animationData: require("./Play.json"),
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Lottie Demo</h1>
+      <div className="container" ref={loader}></div>
+      <div className="container" ref={play}></div>
+      <div className="container" ref={pause}></div>
     </div>
   );
 }
